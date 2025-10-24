06cc6d7b0d3cd0ebf0f70a0dc67bd6101cc6d6cd
Adhil <imdhilu@gmail.com>
2025-10-24 21:19:09 +0530

    Add job to parse endpoint and added Parallel Processing for better processing

--- src/main/java/com/ctrlaltthink/ai_resume_parser/controller/FileUploadController.java
+++ src/main/java/com/ctrlaltthink/ai_resume_parser/controller/FileUploadController.java
@@ -1,8 +1,10 @@
 package com.ctrlaltthink.ai_resume_parser.controller;
 
 
+import com.ctrlaltthink.ai_resume_parser.service.BackgroundProcessingService;
 import com.ctrlaltthink.ai_resume_parser.service.FileParserService;
 import com.ctrlaltthink.ai_resume_parser.service.ResumeScoringEngine;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +18,47 @@ import java.io.InputStream;
 @RequestMapping("/api/files")
 public class FileUploadController {
 
-    @Autowired
     private FileParserService fileParserService;
+    private ResumeScoringEngine resumeScoringEngine;
+    private final BackgroundProcessingService backgroundProcessingService;
 
     @Autowired
-    private ResumeScoringEngine resumeScoringEngine;
+    public FileUploadController(FileParserService fileParserService,ResumeScoringEngine resumeScoringEngine, BackgroundProcessingService backgroundProcessingService) {
+        this.fileParserService= fileParserService;
+        this.resumeScoringEngine= resumeScoringEngine;
+        this.backgroundProcessingService = backgroundProcessingService;
+    }
+
+    @PostMapping("/parallel-parse")
+    public ResponseEntity<String> parseFileAsync(@RequestParam("file") MultipartFile file,
+                                                 @RequestParam(value = "jobDescription", required = false) String jobDescription) {
+        String taskId = backgroundProcessingService.submit(file, jobDescription);
+        return ResponseEntity.accepted().body("taskId:" + taskId);
+    }
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity<?> getStatus(@PathVariable("id") String id) {
+        System.out.println("getStatus: Job Description Received: " + id);
+        BackgroundProcessingService.TaskResult result = backgroundProcessingService.getStatus(id);
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
 
     @PostMapping("/parse")
-    public ResponseEntity<String> parseFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> parseFile(@RequestParam("file") MultipartFile file, @RequestParam("jobDescription") String jobDescription) {
         try {
             String content = fileParserService.extractMetadata(file.getInputStream());
-            return ResponseEntity.ok(content);
+            double score = resumeScoringEngine.calculateScore(content,jobDescription.trim());
+            return ResponseEntity.ok("Total Score For Resume is " + score);
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error parsing file: " + e.getMessage());
         }
     }
 
-    @PostMapping(
-            value = "/parse2",
-            consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE
-    )
+    //Testing: Simplified endpoint for raw byte stream upload
+    @PostMapping(value = "/parse2", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<String> parseFile(@RequestBody byte[] fileBytes) {
         try (InputStream inputStream = new ByteArrayInputStream(fileBytes)) {
             String content = fileParserService.extractMetadata(inputStream);
